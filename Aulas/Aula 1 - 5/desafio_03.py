d0e0184316cf5be8ac30f89e15a35a5c61a3a82b
Andre_Filip3 <andrezinzikin@gmail.com>
2024-09-07 14:20:07 -0300

    comit_exercicios

---
+++ Aulas/Aula 1 - 5/desafio_03.py
@@ -0,0 +1,17 @@
+num1 = int (input('Primeiro número: '))
+num2 = int (input('Segundo número: '))
+soma = num1 + num2
+
+print('A soma é:', soma)
+
+'''
+Exemplo de cast/casting/conversão de tipo:
+
+num1 = input('Primeiro número: ') - Aqui é string
+entrada1 = int(num1) - Aqui é um número inteiro
+num2 = input('Segundo número: ') - Aqui é string
+entrada2 = int(num2) - Aqui é um número inteiro
+soma = entrada1 + entrada2 - Soma dos numeros inteiros
+
+print('A soma é:', soma) - Apresenta a soma das entradas
+print(num1 + num2) - Concatena as strings que foram digitadas'''
---
+++ Exercicios/ex002.py
@@ -0,0 +1,3 @@
+nome = input('Qual é o seu nome? ')
+# As { } serão substituidas pela variável escolhida, utilizando .format()
+print ('É um prazer te conhecer, {}!'.format(nome))
