76cccf1f3b2754c8097d644fabbda36b0d54531e
Andre_Filip3 <andrezinzikin@gmail.com>
2024-09-08 17:52:21 -0300

    comit da aula, praticando

---
+++ Aulas/Aula 7/exemplo1.py
@@ -0,0 +1,47 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+soma = n1+n2
+subtracao = n1-n2
+multiplicacao = n1*n2
+divisao = n1/n2
+divint = n1//n2
+modulo = n1%n2
+expon = n1**n2
+
+print('A soma é: {}'.format(soma), end=' -> ')
+print('A subtração é: {}'.format(subtracao), end=' -> ')
+print('A multiplicação é: {}'.format(multiplicacao))
+print('A divisão é: {:.3f}'.format(divisao))
+print('A divisão inteira é: {}'.format(divint))
+print('O resto da divisão é: {}'.format(modulo))
+print('A potên\ncia é: {}'.format(expon))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''nome = input('Qual é o seu nome? ')
+(O que está dentro das chaves vai preencher com o que você colocar dependendo do numero que colocar)
+print('Prazer em te conhecer {:-^20}!'.format(nome))'''
